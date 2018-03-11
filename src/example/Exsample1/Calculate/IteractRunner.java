ce17ec5af630693bce31037be42b1821b1acac8b
Bogdan Pisarenko <boblinger@gmail.com>
2018-03-11 10:57:49 +0200

    Doing Iterator

--- src/example/Exsample1/Calculate/IteractRunner.java
+++ src/example/Exsample1/Calculate/IteractRunner.java
@@ -1,4 +1,29 @@
 package example.Exsample1.Calculate;
 
+import java.util.Scanner;
+
+/**
+ * Class of the calculator start with user input
+ */
 public class IteractRunner {
+
+    public static void main(String[] args) {
+
+        try (Scanner reader = new Scanner(System.in)) {
+            Calculator calc = new Calculator();
+            String exit = "n";
+            while (!exit.equals("y")) {
+                System.out.println("Enter first argument");
+                String first = reader.next();
+                System.out.println("Enter second argument");
+                String second = reader.next();
+                calc.add(Integer.parseInt(first), Integer.parseInt(second));
+                System.out.println("Result" + calc.getResult());
+                calc.clearResult();
+                System.out.println("Exit? : (y/n)");
+                exit = reader.next();
+            }
+        }
+
+    }
 }
